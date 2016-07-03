7d49e30db51b455dbdbdcd15f259179d4938b530
Idan Stark <Idan Stark>
2016-07-03 16:57:50 +0300

    Added name to algorithm

--- AlgorithmsDatabase/src/model/Algorithm.java
+++ AlgorithmsDatabase/src/model/Algorithm.java
@@ -4,4 +4,18 @@ package model;
  * Created by Idan Stark on 02/07/16.
  */
 public class Algorithm {
+
+    private String name;
+
+    public Algorithm(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
