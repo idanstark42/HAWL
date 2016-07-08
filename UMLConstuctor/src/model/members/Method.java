34aab65b14e6404f8b97ab667bea4a48a081dd0f
Idan Stark <Idan Stark>
2016-07-08 22:50:13 +0300

    Changed field Algorithm type in class Action from Algorithm to Action.

--- UMLConstuctor/src/model/members/Method.java
+++ UMLConstuctor/src/model/members/Method.java
@@ -1,12 +1,12 @@
 package model.members;
 
-import model.Algorithm;
+import model.Action;
 
 /**
  * Created by Idan Stark on 08/07/16.
  */
 public class Method extends Member{
 
-    private Algorithm algorithm;
+    private Action function;
 
 }
