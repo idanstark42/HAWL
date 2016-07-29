2a9a665e26c2ef11e3db2bf282c804aa29b0c91c
Idan Stark <Idan Stark>
2016-07-29 22:26:27 +0300

    Add class and interface models.

---
+++ UMLConstuctor/src/model/classes/Class.java
@@ -0,0 +1,18 @@
+package model.classes;
+
+import model.members.Field;
+import model.members.Member;
+
+import java.util.List;
+
+/**
+ * Created by Idan Stark on 29/07/16.
+ */
+public class Class {
+
+    private Class parent;
+    private List<Interface> abilities;
+
+    private List<Member> fields;
+
+}
---
+++ UMLConstuctor/src/model/classes/Interface.java
@@ -0,0 +1,17 @@
+package model.classes;
+
+import model.members.Field;
+import model.members.Member;
+
+import java.util.List;
+
+/**
+ * Created by Idan Stark on 29/07/16.
+ */
+public class Interface{
+
+    private Interface parent;
+
+    private List<Member> fields;
+
+}
