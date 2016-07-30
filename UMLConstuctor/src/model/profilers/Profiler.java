6c07064518ad3df28b2e79210a57d7dcbc777343
Idan Stark <Idan Stark>
2016-07-30 13:50:08 +0300

    Added profilers and ClassConstructor

--- UMLConstuctor/src/model/profilers/Profiler.java
+++ UMLConstuctor/src/model/profilers/Profiler.java
@@ -1,7 +1,12 @@
 package model.profilers;
 
+import model.classes.Class;
+
 /**
  * Created by Idan Stark on 29/07/16.
  */
 public abstract class Profiler {
+
+    public abstract void change(Class clazz);
+
 }
