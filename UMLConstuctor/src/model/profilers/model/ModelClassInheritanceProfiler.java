72dcac9550170919892f9c8d7fd424c695c81d9f
Idan Stark <Idan Stark>
2016-07-30 19:47:55 +0300

    Added model profiler classes.

---
+++ UMLConstuctor/src/model/profilers/model/ModelClassInheritanceProfiler.java
@@ -0,0 +1,14 @@
+package model.profilers.model;
+
+import model.classes.Class;
+import model.profilers.Profiler;
+
+/**
+ * Created by Idan Stark on 30/07/16.
+ */
+public class ModelClassInheritanceProfiler extends Profiler {
+    @Override
+    public void change(Class clazz) {
+
+    }
+}
---
+++ UMLConstuctor/src/model/profilers/model/ModelPropertyProfiler.java
@@ -0,0 +1,16 @@
+package model.profilers.model;
+
+import model.classes.Class;
+import model.profilers.Profiler;
+
+/**
+ * Created by Idan Stark on 30/07/16.
+ */
+public class ModelPropertyProfiler extends Profiler {
+
+    @Override
+    public void change(Class clazz) {
+
+    }
+
+}
