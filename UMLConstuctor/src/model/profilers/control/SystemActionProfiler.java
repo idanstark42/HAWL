4eb528ca5baaa6c7803c9f4fae6dd6be8250d638
Idan Stark <Idan Stark>
2016-07-30 20:07:33 +0300

    Added the control profilers.

--- UMLConstuctor/src/model/profilers/control/SystemActionProfiler.java
+++ UMLConstuctor/src/model/profilers/control/SystemActionProfiler.java
@@ -1,7 +1,16 @@
 package model.profilers.control;
 
+import model.classes.Class;
+import model.profilers.Profiler;
+
 /**
  * Created by Idan Stark on 30/07/16.
  */
-public class SystemActionProfiler {
+public class SystemActionProfiler extends Profiler {
+
+    @Override
+    public void change(Class clazz) {
+
+    }
+
 }
